d73a1c05bf754ff4500cca0e4b26242cde933926
williamgilpin <williamgilpin@gmail.com>
2023-10-28 02:54:05 -0500

    latest

--- talks/convert_to_html.sh
+++ talks/convert_to_html.sh
@@ -5,5 +5,5 @@ mkdir -p html_static
 
 # Loop through each .ipynb file and convert it to HTML in the subdirectory
 for i in *.ipynb; do
-  jupyter nbconvert --to html "$i" --output-dir=html_output
+  jupyter nbconvert --to html "$i" --output-dir=html_static
 done
