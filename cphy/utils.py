b9b281852d0f39c06d695d1d243ee24703509abb
williamgilpin <williamgilpin@gmail.com>
2025-08-18 18:16:58 -0400

    latest book

---
+++ cphy/utils.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+from hashlib import sha256
+
+def hash_string(s: str) -> str:
+    """
+    Hashes a string using SHA-256.
+
+    Args:
+        s (str): The string to hash.
+
+    Returns:
+        str: The SHA-256 hash of the string.
+    """
+    return sha256(s.encode()).hexdigest()
+
+def fetch_userstring():
+    """
+    Fetches the user's email address if running in Google Colab, otherwise returns the local username.
+
+    Returns:
+        str: The user's email address if running in Google Colab, otherwise the local username.
+            If there is an error fetching the user email, returns "no_user".
+
+    """
+    
+    # Detect if running in Google Colab
+    in_colab = "google.colab" in sys.modules
+    
+    if in_colab:
+
+        print("Running in Google Colab." +
+            "You will need to authenticate your Google account (prompted by the browser)" +
+            "to use this function.", 
+            flush=True
+        )
+        try:
+            # In Colab: get Google account email
+            from google.colab import auth
+            auth.authenticate_user()
+            
+            import google.auth
+            from google.auth.transport.requests import Request
+            from googleapiclient.discovery import build
+            
+            creds, _ = google.auth.default()
+            creds.refresh(Request())
+            service = build('oauth2', 'v2', credentials=creds)
+            user_info = service.userinfo().get().execute()
+            return user_info['email']
+        except Exception as e:
+            print(f"Error fetching user email: {e}", flush=True)
+            return "no_user"
+    else:
+        # Local: return username
+        try:
+            username = os.getlogin()
+        except Exception:
+            username = os.environ.get("USER") or os.environ.get("USERNAME")
+        return username
---
+++ talks/front.md
@@ -0,0 +1,6 @@
+
+# Computational Physics
+
+This course aims to provide a very broad survey of computational methods that are particularly relevant to modern physics research. We will aim to cover efficient algorithm design and performance analysis, traditional numerical recipes such as integration and matrix manipulation, and emerging methods in data analysis and machine learning. Our goal by the end of the class will be to feel comfortable approaching diverse, open-ended computational problems that arise during research, and to be ready to design and share new algorithms with the broader research community.
+
+This book is adapted from the course materials for the Computational Physics course taught by at the University of Texas at Austin. The course website, including runnable notebooks and code for all chapters, can be found online at: [https://github.com/williamgilpin/cphy](https://github.com/williamgilpin/cphy).
