f0d6e8ebb8630878b0801289bdd55b4ffab9fff6
williamgilpin <williamgilpin@gmail.com>
2025-10-16 21:02:15 -0500

    latest

--- cphy/template.py
+++ cphy/template.py
@@ -7,3 +7,14 @@ plt.rcParams['font.family'] = 'Helvetica'
 plt.rcParams['image.cmap'] = 'PuBu'
 plt.rcParams['axes.prop_cycle'] = plt.cycler(color=[[1.0, .3882, .2784]])
 plt.rcParams['lines.markersize'] = 10
+
+
+## In-notebook video settings
+plt.rcParams["animation.writer"] = "ffmpeg"
+plt.rcParams["animation.bitrate"] = 800                     # kb/s cap
+plt.rcParams["animation.ffmpeg_args"] = [                   # finer control
+    "-crf", "24",        # 18–28 is common; larger = smaller file
+    "-preset", "slow",   # slower = better compression
+    "-pix_fmt", "yuv420p"
+]
+plt.rcParams["animation.embed_limit"] = 25
